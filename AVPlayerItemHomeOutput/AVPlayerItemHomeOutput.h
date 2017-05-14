8f978feb38c45d23fc9a0d58e252f820760688cf
Alex Rupérez <alexruperez@gmail.com>
2017-05-14 18:01:49 +0200

    Initial Commit

---
+++ AVPlayerItemHomeOutput/AVPlayerItemHomeOutput.h
@@ -0,0 +1,19 @@
+//
+//  AVPlayerItemHomeOutput.h
+//  AVPlayerItemHomeOutput
+//
+//  Created by Alex Rupérez on 14/5/17.
+//  Copyright © 2017 alexruperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVPlayerItemHomeOutput.
+FOUNDATION_EXPORT double AVPlayerItemHomeOutputVersionNumber;
+
+//! Project version string for AVPlayerItemHomeOutput.
+FOUNDATION_EXPORT const unsigned char AVPlayerItemHomeOutputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVPlayerItemHomeOutput/PublicHeader.h>
+
+
